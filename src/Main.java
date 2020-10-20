a4dda26bc7c1cb15f70eab6721b6285b0b9f05e9
GoldenCorgi <42860714+GoldenCorgi@users.noreply.github.com>
2020-10-20 15:27:47 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int choice;
+        Scanner sc = new Scanner(System.in);
+        do {
+//            System.out.println("(1) Show number of empty seats");
+//            System.out.println("(2) Show the list of empty seats");
+//            System.out.println("(3) Show the list of seat assignments by seat ID");
+//            System.out.println("(4) Show the list of seat assignments by customer ID");
+//            System.out.println("(5) Assign a customer to a seat");
+//            System.out.println("(6) Remove a seat assignment");
+//            System.out.println("(7) Exit");
+//            System.out.print("Enter the number of your choice: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1: System.out.println("1...");
+                    break;
+                case 2: System.out.println("2...");
+                    break;
+                case 3: System.out.println("3...");
+                    break;
+                case 4: System.out.println("4...");
+                    break;
+                case 5:
+                    System.out.println("5...");
+                    break;
+                case 6:
+                    System.out.println("6...");
+                    break;
+
+                case 7: System.out.println("Program terminating...");break;
+            }
+        } while (choice<7);
+    }
+
+}
---
+++ src/MainTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void evaluatesExpression() {
+        new Main();
+
+        assertEquals(6, 5);
+    }
+}
+
