75f45c9cfa354cd9e25da95fe77a7f8b4ccdfd73
GoldenCorgi <42860714+GoldenCorgi@users.noreply.github.com>
2020-10-20 15:53:19 +0800

    updated paths

--- src/main/java/mystars/Main.java
+++ src/main/java/mystars/Main.java
@@ -1,3 +1,5 @@
+package mystars;
+
 import java.util.Scanner;
 
 public class Main {
---
+++ src/test/java/mystars/MainTest.java
@@ -0,0 +1,12 @@
+package mystars;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
