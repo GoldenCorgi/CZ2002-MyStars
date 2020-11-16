023b89e68d36d22e47278af423dec7a1714af3e1
Pandakick <joeyshi@live.com>
2020-11-17 03:40:19 +0800

    course updated

--- src/main/java/mystars/courses/Course.java
+++ src/main/java/mystars/courses/Course.java
@@ -8,98 +8,78 @@ import java.util.ArrayList;
 public class Course implements Serializable{
     private static final long serialVersionUID = 20;
     private String courseName;
-    private ArrayList<CourseComponent> courseComponent = new ArrayList<CourseComponent>();
+    private int courseCode;
+    private ArrayList<CourseComponent> courseComponents = new ArrayList<CourseComponent>();
 
-    public Course(String courseName, int lectureVacancies) {
+    /**
+     * Contructor to create Course object based on parameters given. Creates courses with only lectures
+     *
+     * @param courseName        The name of the course.
+     * @param courseCode The code number of the course.
+     * @param courseVacancies The number of vacancies for a course.
+     */
+
+    public Course(String courseName, int courseCode, int courseVacancies) {
         this.courseName = courseName;
+        this.courseCode = courseCode;
 
-        CourseComponent lecture = new Lecture(lectureVacanies);
-        courseComponent.add(lecture);
+        CourseComponent lecture = new Lecture(courseVacancies);
+        courseComponents.add(lecture);
     }
 
     /**
-     * Constructor to create Course object based on parameters given. Creates course
-     * that has both lectures and tutorials.
+     * Contructor to create Course object based on parameters given. Creates courses with only lectures and tutorials/laboratory
      *
-     * @param courseName             The name of the course.
-     * @param lectureVacancies       The number of slots for a lecture group.
-     * @param numberOfTutorialGroups The number of tutorial groups of the course.
-     * @param slotsPerTutGroup       The number of slots for a tutorial group.
+     * @param courseName The name of the course.
+     * @param courseCode The code number of the course.
+     * @param courseVacancies The number of vacancies for a course.
+     * @param numberOfIndexes The number of indexes for a course.
+     * @param slotsPerIndex The number of slots for an index of a course.
+     * @param isTutorialOnly Boolean: true if is tutorial.
      */
 
-    public Course(String courseName, int lectureVacancies, int numberOfTutorialGroups, int slotsPerTutGroup) {
-        this(courseName, lectureVacancies);
+    public Course(String courseName, int courseCode, int courseVacancies, int numberOfIndexes, int slotsPerIndex, boolean isTutorialOnly) {
+        this(courseName, courseCode, courseVacancies);
 
-        CourseComponent tutorial = new Tutorial(numberOfTutorialGroups, slotsPerTutGroup);
-        courseComponent.add(tutorial);
-    }
-
-    /**
-     * Constructor to create Course object based on parameters given. Creates course
-     * that have lectures, tutorials and labs.
-     *
-     * @param courseName             The name of the course.
-     * @param lectureVacancies       The number of slots for a lecture group.
-     * @param numberOfTutorialGroups The number of tutorial groups of the course.
-     * @param slotsPerTutGroup       The number of slots for a tutorial group.
-     * @param numberOfLabGroups      The bumber of lab groups of the course.
-     * @param slotsPerLabGroup       The number of slots for a lab group.
-     */
-
-    public Course(String courseName, int lectureVacancies, int numberOfTutorialGroups, int slotsPerTutGroup,
-                  int numberOfLabGroups, int slotsPerLabGroup) {
-        this(courseName, courseCoordinator, lectureVacancies, numberOfTutorialGroups, slotsPerTutGroup);
-
-        CourseComponent lab = new Laboratory(numberOfLabGroups, slotsPerLabGroup);
-        CourseComponent.add(lab);
-    }
-
-    /**
-     * This method is used to register student for a course taht have only lectures.
-     * By assumption, there is only one lecture group.
-     *
-     * @param s The student object.
-     */
-
-    public void registerStudent(Student s) {
-        this.courseComponent.get(0).addStudent(s, 1);
-    }
-
-    /**
-     * This method is used to register student for a course that have both lectures
-     * and labs.
-     *
-     * @param s        The student object.
-     * @param tutGroup The tutorial group of the course for which a student wants to
-     *                 register.
-     */
-
-    public void registerStudent(Student s, int tutGroup) {
-        this.addStudent(s);
-        this.courseComponent.get(1).addStudent(s, tutGroup);
-    }
-
-    /**
-     * This method is used to register student for a course that have lectures,
-     * tutorials and labs.
-     *
-     * @param s        The student object.
-     * @param tutGroup The tutorial group of the course for which a student wants to
-     *                 register.
-     * @param labGroup The lab group of the course for which a student wants to
-     *                 register.
-     */
-
-    public void registerStudent(Student s, int tutGroup, int labGroup) {
-        this.addedStudents(s, tutGroup);
-        this.courseComponent.get(2).addStudent(s, labGroup);
-    }
-
-    public boolean haveVacancies() {
-        boolean vacant = true;
-        for (CourseComponent cc : this.courseComponent) {
-            vacant &= cc.haveVacancies();
+        if (isTutorialOnly) {
+            CourseComponent tutorial = new Tutorial(numberOfIndexes, slotsPerIndex);
+            courseComponents.add(tutorial);
         }
-        return vacant;
+        else {
+            CourseComponent laboratory = new Laboratory(numberOfIndexes, slotsPerIndex);
+            courseComponents.add(laboratory);
+        }
+    }
+
+    /**
+     * Contructor to create Course object based on parameters given. Creates courses with only lectures, tutorials and labs
+     *
+     * @param courseName        The name of the course.
+     * @param courseCode The code number of the course.
+     * @param courseVacancies The number of vacancies for a course.
+     * @param numberOfIndexes The number of indexes for a course.
+     * @param slotsPerIndex The number of slots for an index of a course.
+     */
+
+    public Course(String courseName, int courseCode, int courseVacancies, int numberOfIndexes, int slotsPerIndex) {
+        this(courseName, courseCode, courseVacancies, numberOfIndexes, slotsPerIndex);
+
+        CourseComponent laboratory = new Laboratory(numberOfIndexes, slotsPerIndex);
+        courseComponents.add(laboratory);
+    }
+
+    /* get courseName */
+    public String getCourseName() {
+        return this.courseName;
+    }
+
+    /* get courseName */
+    public int getCourseCode() {
+        return this.courseCode;
+    }
+
+    /* get courseName */
+    public ArrayList<CourseComponent> getCourseComponents() {
+        return this.courseComponents;
     }
 }
