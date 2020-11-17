1d59e2a976f9bce862f7dcad8ace547eef458f73
qy2000 <49312264+qy2000@users.noreply.github.com>
2020-11-17 14:37:26 +0800

    updated student and main

--- src/main/java/mystars/Main.java
+++ src/main/java/mystars/Main.java
@@ -2,6 +2,8 @@ package mystars;
 import mystars.login.Login;
 import mystars.login.UserList;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -31,13 +33,93 @@ public class Main {
 
 
         if (rolename.equals("Student")) {
-            // TODO add student actions
             System.out.println("Welcome Student");
+            // TODO add student actions
+            int choice;
+            System.out.println("(1) Add course");
+            System.out.println("(2) Drop course");
+            System.out.println("(3) Check/Print Courses Registered");
+            System.out.println("(4) Check Vacancies Available");
+            System.out.println("(5) Change Index Number of Course");
+            System.out.println("(6) Swop Index Number with Another Student");
+            System.out.println("(7) Exit");
+            Student Student1 = new Student();
+            Scanner sc = new Scanner(System.in);
+            do {
+                System.out.println("Enter the number of your choice: ");
+                choice = sc.nextInt();
+                switch (choice) {
+                    case 1:
+                        System.out.println("Please enter index number: ");
+                        int index = sc.nextInt();
+                        String indexNum = String.valueOf(index);
+                        System.out.println("Enter 'OK' to confirm add course");
+                        String confirm1 = sc.nextLine();
+                        Student1.addCourse(indexNum, confirm1);
+                        break;
+                    case 2:
+                        System.out.println("These are the courses that you have registered:");
+                        //Display the course index, course code, and status of the course
+                        HashMap coursesRegistered = Student1.getCourses();
+                        System.out.println(coursesRegistered);
+                        System.out.println("Enter what Course Index you want to drop: ");
+                        int index1 = sc.nextInt();
+                        String indexNum1 = String.valueOf(index1);
+                        System.out.println("Enter 'OK' to confirm drop course");
+                        String confirm2 = sc.nextLine();
+                        Student1.dropCourse(indexNum1, confirm2);
+                        break;
+                    case 3:
+                        Student1.getCourses();
+                        break;
+                    case 4:
+                        System.out.println("Enter Course Index: ");
+                        int index2 = sc.nextInt();
+                        String indexNum2 = String.valueOf(index2);
+
+                        break;
+                    case 5:
+                        System.out.println("Enter Current Index: ");
+                        int index3 = sc.nextInt();
+                        String indexNum3 = String.valueOf(index3);
+                        System.out.println("Enter New Index: ");
+                        int index4 = sc.nextInt();
+                        String indexNum4 = String.valueOf(index4);
+                        System.out.println("Enter 'OK' to confirm drop course");
+                        String confirm3 = sc.nextLine();
+                        Student1.changeIndex(indexNum3, indexNum4, confirm3);
+
+                        break;
+                    case 6:
+                        System.out.println("Enter Your Index: ");
+                        int index5 = sc.nextInt();
+                        String indexNum5 = String.valueOf(index5);
+                        System.out.println("Enter Peer's Username: ");
+                        String peerUN = sc.nextLine();
+                        System.out.println("Enter Peer's Password: ");
+                        String peerPW = sc.nextLine();
+                        System.out.println("Enter Peer's Index: ");
+                        int index6 = sc.nextInt();
+                        String indexNum6 = String.valueOf(index6);
+                        System.out.println("Enter 'OK' to confirm drop course");
+                        String confirm4 = sc.nextLine();
+                        // check if peerStudID and peerPassword is valid
+                        Student1.swapIndex(indexNum5, indexNum6, confirm4);
+
+
+                        break;
+                    case 7:
+                        System.out.println("Program terminating..");
+
+                }
+            } while(choice < 7);
         }
+
         else if (rolename.equals("Admin")) {
             // TODO add admin actions
             System.out.println("Welcome Admin");
         }
+
         else {
             System.out.println("Unknown Role");
         }
--- src/main/java/mystars/Student.java
+++ src/main/java/mystars/Student.java
@@ -1,5 +1,185 @@
 package mystars;
 
-public class Student {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.Map;
 
+public class Student {
+    private String matricNo;
+    private String studName;
+    private String studServerName;
+    private int numberOfCoursesRegistered;
+    private HashMap<String, String> studCourses= new HashMap<>();
+    // private HashMap<String, Object> studCourses= new HashMap<>(); use Course as object?
+    private final int MAXCOURSES=6;
+
+    public Student() {}
+
+    //public String getStudName(studServerName) {return studName;}
+    //public String getMatricNo(studServerName) { return matricNo;}
+
+    public boolean hasCourse(String courseCode) {
+        if (studCourses.containsKey(courseCode)){
+            return true;
+        }
+        else { return false;}
+    }
+    public void addCourse(String courseIndex, String confirmInput) {
+        //if (courseIndex) in txt file (ie. valid)
+        // extract courseCode from the .txt file using courseIndex as key
+        // getCourseCode using courseIndex from Courses class?
+        // hasVacancies check using Courses class?
+        //hasVacancy = courseList.hasVacancies(courseIndex)
+        boolean hasVacancy = true;
+        //courseCode = courseList.getCourseCode(courseIndex);
+        String courseCode = "CZ2002";
+        if(!hasCourse(courseCode)) {
+            if (hasVacancy) {
+                if (numberOfCoursesRegistered <= MAXCOURSES) {
+                    //display the index info
+                    System.out.println("Enter 'OK' to confirm add course");
+                    boolean confirmation = Confirmation(confirmInput);
+                    if(confirmation) {
+                        System.out.println("This index has now been added to your courses!");
+                        studCourses.put(courseCode, courseIndex);
+                        // reduce vacancy of this index
+                    }
+                    else{
+                        System.out.println("This index is not added.");
+                    }
+                }
+            }
+        }
+        //} // do i need to input an else failed statement?
+    }
+
+    public boolean Confirmation(String confirm) {
+        if(confirm.toUpperCase() == "OK") { return true; }
+        else { return false;}
+    }
+
+    public void dropCourse(String courseIndex, String confirmInput) {
+        System.out.println("These are the courses that you have registered:");
+        //Display the course index, course code, and status of the course
+        System.out.println("Enter what Course Index you want to drop: ");
+        //Scanner sc = new Scanner(System.in);
+        //courseIndex = sc.nextInt();
+        //if (courseIndex) in list of studCourses
+        // extract courseCode from the .txt file using courseIndex as key
+
+        //courseCode = courseList.getCourseCode(courseIndex);
+        String courseCode = "CZ2002";
+        if(hasCourse(courseCode)) {
+            //display the index info (course index, course code, course type, course status)
+            boolean confirmation = Confirmation(confirmInput);
+            if(confirmation) {
+                System.out.println("This index has now been dropped!");
+                studCourses.remove(courseCode, courseIndex);
+                // reduce vacancy of this index
+            }
+            else{
+                System.out.println("This index is not added.");
+            }
+        }
+        //}
+    }
+
+    public HashMap getCourses() {
+        // using hashmap (Qingyi)
+        /* Display content using Iterator*/
+        Set set = studCourses.entrySet();
+        Iterator iterator = set.iterator();
+        while(iterator.hasNext()) {
+            Map.Entry mapEntry = (Map.Entry)iterator.next();
+            System.out.print("Course code is: "+ mapEntry.getKey() + " & Course index is: ");
+            System.out.println(mapEntry.getValue());
+        }
+
+        return studCourses;
+
+    }
+
+    // hasVacancies can be found using Courses class?
+    //public boolean hasVacancies(courseIndex){
+    //    if (checkVacancies > 0){return true;}
+    //    else{return false;}
+    //}
+
+    public void displayVacancies(String courseIndex){
+        // use get methods from Courses class to get course index info?
+        // display the course index info
+        // display places available and length of waitlist, vacancies
+    }
+
+    //public boolean checkTimingClash(int courseIndex, studCourses) {}
+
+    public int getNumberOfCoursesRegistered(HashMap studCourses){
+        this.numberOfCoursesRegistered = studCourses.size();
+        return  numberOfCoursesRegistered;
+    }
+
+
+//    public void checkVacancies(int courseIndex){
+//        //justina
+//        System.out.println("The number of vacancies for the course index %d is %d",
+//                courseIndex, getNumCourseIndexAvail);
+//    }
+
+    public void changeIndex(String oldIndex, String newIndex, String confirmInput){
+        // from Courses class, getCourseCode of newIndex
+        String courseCodeNew="CZ2002";
+        //courseCodeNew = courseList.getCourseCode(oldIndex);
+        // compare if they are from the same course
+        // from Courses class, check for vacancy of newIndex
+
+        if (!studCourses.containsValue(newIndex)){
+            // display course info of old index
+            // display course info of new index
+            boolean confirmation = Confirmation(confirmInput);
+            if(confirmation) {
+                System.out.println("Index Number" + oldIndex+ " changed to "+ newIndex);
+                studCourses.values().remove(oldIndex);
+                studCourses.put(courseCodeNew, newIndex);
+            }
+            else{
+                System.out.println("This index is not changed.");
+            }
+            // increase vacancy of oldIndex using Courses class
+            // decrease vacancy of newIndex using Courses class
+        }
+
+    }
+
+    public void swapIndex(String oldCourseIndex, String newCourseIndex, String confirmInput){
+        // from Courses class, getCourseCode of newIndex
+        String courseCode="CZ2002";
+        //courseCode = courseList.getCourseCode(oldIndex);
+        if (!oldCourseIndex.equals(newCourseIndex)){
+            boolean confirmation = Confirmation(confirmInput);
+            if(confirmation) {
+                // increase vacancy of oldIndex using Courses class
+                // decrease vacancy of newIndex using Courses class
+                studCourses.values().remove(oldCourseIndex);
+                studCourses.put(courseCode,newCourseIndex);
+                System.out.println("Index Number" + oldCourseIndex+ " changed to "+ newCourseIndex);
+            }
+            else{
+                System.out.println("This index is not changed.");
+            }
+            // increase vacancy of oldIndex using Courses class
+            // decrease vacancy of newIndex using Courses class
+            studCourses.values().remove(oldCourseIndex);
+            studCourses.put(courseCode,newCourseIndex);
+
+        }
+        // in the main function, create another student class for the peer, to swap index for peer
+
+        // check if current index is not equal to newCourseIndex
+        // check if peerStudID and peerPassword is valid
+        // put oldCourseIndex to temp int, add newCourseIndex to studCourses
+        // put temp int to peerStudCourses
+    }
 }
+
